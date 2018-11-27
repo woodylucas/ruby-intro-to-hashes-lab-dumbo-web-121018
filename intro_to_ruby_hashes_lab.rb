f0c2f1a83db9bf7ef8e672f1c041a71f025dffbe
Woody Lucas <wlucas0311@gmail.com>
2018-11-27 06:47:27 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -59,7 +59,7 @@ def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 	monopoly = {
 	  :railroads => { pieces: 4, 
-	                 names: { reading_railroad: {},
+	                 names: { reading_railroad: {"mortgage_value" => "$100"},
 	                 pennsylvania_railroad: {},
 	                 b_and_o_railroad: {},
 	                 shortline: {}
