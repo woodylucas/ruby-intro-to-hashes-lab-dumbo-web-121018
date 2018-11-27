b571354b64fc23eb0b3501844dce9763f5eeac71
Woody Lucas <wlucas0311@gmail.com>
2018-11-27 06:44:37 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -56,7 +56,7 @@ def monopoly_with_fourth_tier
 	  :railroads => { pieces: 4, 
 	                 names: { reading_railroad: {},
 	                 pennsylvania_railroad: {},
-	                 b_and_o_railroad: {}
+	                 b_and_o_railroad: {},
 	                 shortline: {}
 	                   
 	                 },
