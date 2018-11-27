b746821569f9e6e8d8c4d9cc41c7f4b3d2a51e4c
Woody Lucas <wlucas0311@gmail.com>
2018-11-27 06:33:20 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	  :railroads => {pieces: => 4, 
+	  :railroads => { pieces: => 4, 
 	                 names: => {},
 	                 rent_in_dollars: => {}
 	    
